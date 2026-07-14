16eb590a1cdd0b80ec366151c752879a641cf9aa
Sapnap67 <vickiedaiyc@163.com>
2026-07-14 14:50:52 +0800

    feat: index_new.html - rebuilt from working MVP + pomodoro + ESP32 + light theme

--- 启动.bat
+++ 启动.bat
@@ -4,11 +4,11 @@ title FocusBuddy
 cd /d "%~dp0"
 
 echo ================================
-echo   🐱 FocusBuddy 完整版
-echo   浏览器打开: http://localhost:8080
+echo   🐱 FocusBuddy
+echo   浏览器打开: http://localhost:8080/index_new.html
 echo   按 Ctrl+C 停止
 echo ================================
 
-start "" "http://localhost:8080"
+start "" "http://localhost:8080/index_new.html"
 python -m http.server 8080
 pause
